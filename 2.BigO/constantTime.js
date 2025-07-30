dbbdf1936585a5ccb7fbe733b18d09307ba96f0c
Harshad <harshadadagale05@gmail.com>
2025-07-30 12:43:33 +0530

    Formatted Properly

--- 2.BigO/constantTime.js
+++ 2.BigO/constantTime.js
@@ -3,4 +3,5 @@ const numbers = [1, 2, 3, 4, 5];
 // O(1)
 
 const getElement = (arr, index) => arr[index]; 
-    console.log(getElement(numbers, 2)); // Output: 3   
+
+console.log(getElement(numbers, 2)); // Output: 3
